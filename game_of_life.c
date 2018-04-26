04aa63a619147f44ea255f9db9d3ad69be91307f
Mythie <mythiedev@gmail.com>
2018-04-27 01:56:31 +1000

    wot

--- game_of_life.c
+++ game_of_life.c
@@ -1,113 +1,147 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "game_of_life.h"
 
-typedef struct gol{
-	int **board;
-	size_t size;
-}gol;
+#define DEFAULT_GOL_SIZE 20
 
 /**
  * Create a board with the default sizing (20x20)
  * @return gol[20][20]  The board in the default sizing
  */
+
 gol* create_default_gol() {
-	// Create the gol instance
-	gol** new_gol;
-	// Set the size of the board
-	new_gol->size = 20;
-	// Dynamically fill the rows with zero's
-	new_gol->board = malloc(new_gol->size * sizeof(int *));
-	if(new_gol->board == NULL) {
-		// TODO: Catch some errors
-		return NULL;
-	}
-	// Then dynamically fill the columns with 0's
-	for(int i = 0; i < new_gol->size; i++) {
-		new_gol->board[i] = malloc(new_gol->size * sizeof(int *));
-		if(new_gol->board[i] == NULL) {
-			// TODO: Catch some errors
-			return NULL;
-		}
-	}
-	return new_gol;
+    size_t size = DEFAULT_GOL_SIZE;
+    return create_gol(size);
 }
 
-/**
- * Create a board with the user specified sizing
- * @param size_t 	The size of the board
- * @return gol[size][size] 	The board of the size specified
- */
 gol* create_gol(size_t size) {
-	// Create the gol instance
-	gol* new_gol;
-	// Set the size of the board
-	new_gol->size = size;
-	// Dynamically fill the rows with zero's
-	new_gol->board = malloc(new_gol->size * sizeof(int *));
-	if(new_gol->board == NULL) {
-		// TODO: Catch some errors
-		return NULL;
-	}
-	// Then dynamically fill the columns with 0's
-	for(int i = 0; i < new_gol->size; i++) {
-		new_gol->board[i] = malloc(new_gol->size * sizeof(int *));
-		if(new_gol->board[i] == NULL) {
-			// TODO: Catch some errors
-			return NULL;
-		}
-	}
-	return new_gol;
+    //`gol *new_gol;
+    gol *new_gol = malloc(sizeof(gol*));
+
+    new_gol->size = size;
+    new_gol->board = malloc(sizeof(int *) * new_gol->size);
+    if(new_gol->board == NULL) {
+        printf("Hit Null on malloc 1d");
+        return NULL;
+    }
+
+    for(int i = 0; i < new_gol->size; i++) {
+        new_gol->board[i] = malloc(sizeof(int *) * new_gol->size);
+        if(new_gol->board[i] == NULL) {
+            printf("Hit Null on Malloc 2d");
+            // TODO: Catch some errors
+            return NULL;
+        }
+    }
+
+    // Zero out the board, this is needed since malloc won't always create a zeroed array??
+    // Could use memset but opted against importing another library.
+    for(int i = 0; i < new_gol->size; i++) {
+        for(int j = 0; j < new_gol->size; j++) {
+            new_gol->board[i][j] = 0;
+        }
+    }
+    return new_gol;
 }
 
-/**
- * Remove the board
- * @param gol 	The board to remove
- */
+// TODO: Probably go over every item in "g" and free them
 void destroy_gol(gol* g) {
-	free(g);
+    free(g);
 }
 
-/**
- * This sets the pattern, in reality the pattern is actually passed to it and it's applied to the board?
- * @param gol 	The board
- * @param int[][] 	The pattern to apply?
- */
-void set_pattern(gol* g, int** b) {
+void set_pattern(gol *g, int **b) {
+    // TODO: Further implement this
+    // Thoughts: Iterate over b since we know it's an int sizeof(b)/sizeof(int *)??
+    // This way if we apply a smaller config to a bigger board it will only be applied to
+    // a smaller area of the board as opposed to changing the whole thing :)
 
+    // Could use memset?
+    for(int i = 0; i < g->size; i++) {
+        for(int j = 0; j < g->size; j++) {
+            g->board[i][j] = b[i][j];
+        }
+    }
+    printf("\n");
 }
 
-/**
- * Despite the name this actually progresses the board, evolve would be a more suitable name
- * @param
- */
-void next_pattern(gol* g) {
+void next_pattern(gol *g) {
+    // Create a second board
+    // NOTE: We don't need to zero this one out as we will be using our
+    // reference board and writing values to it accordingly
+    int **b = malloc(g->size * sizeof(int *));
+    // Create the second board continued
+    for(int i = 0; i < g->size; i++) {
+        b[i] = malloc(g->size * sizeof(int *));
+    }
 
+    for(int i = 0; i < g->size; i++) {
+        for (int j = 0; j < g->size; j++) {
+            int sum = neighbour_sum(g, i, j);
+            //printf("board[%d][%d] sum: %d\n", i, j, sum);
+            /*
+             * As per the rules:
+             * Any live cell with 2-3 neighbours lives either through survival
+             * Any dead cell with 3 neighbours gains life through reproduction
+             * anything else will die either from famine or overpopulation
+             */
+            // Alive and has two or three neighbours
+            if((g->board[i][j] == 1) && (sum == 2 || sum == 3)) {
+                // Lives
+                b[i][j] = 1;
+            }
+            // Dead and has exactly three neighbours
+            else if((g->board[i][j] == 0) && (sum == 3)) {
+                // Lives
+                b[i][j] = 1;
+            }
+            // Otherwise
+            else {
+                // Dies (overpopulation or crowding)
+                b[i][j] = 0;
+            }
+        }
+    }
+    // Finalise it by setting the game state we just created on our board.
+    set_pattern(g, b);
 }
 
-/**
- * This takes in a board and a position and checks the neighbors values to calculate their sum (1 or 0)
- * @param gol 	The board
- * @param int 	The x location of the cell
- * @param int 	The y location of the cell
- * @return int 	The amount of alive neighbours next to the cell.
- */
-int neighbour_sum(gol* g, int i, int j) {
-	return 0;
+int neighbour_sum(gol *g, int i, int j) {
+    // Start off with zero and increment as we hit matches
+    if(i >= g->size || j >= g->size) {
+        printf("Neighbour sum out of bounds i: %d\tj: %d\n", i, j);
+        return 0;
+    }
+
+    int sum = 0;
+    int n = (int)g->size;
+    // Top-left
+    sum += g->board[(i-1+n)%n][(j-1+n)%n];
+    // Top center
+    sum += g->board[(i-1+n)%n][j];
+    // Top right
+    sum += g->board[(i-1+n)%n][(j+1)%n];
+    // Right
+    sum += g->board[i%n][(j+1)%n];
+    // Bottom right
+    sum += g->board[(i+1)%n][(j+1)%n];
+    // Bottom Center
+    sum += g->board[(i+1)%n][j];
+    // Bottom Left
+    sum += g->board[(i+1)%n][(j-1+n)%n];
+    // Left
+    sum += g->board[i][(j-1+n)%n];
+    // Return the sum
+    return sum;
 }
 
-/**
- * Prints the current state of the board
- * @param gol  The board
- */
 void print(gol* g) {
-	// For each row?
-	for(int i = 0; i < g->size; i++) {
-		// For each column
-		for(int j = 0; j < g->size; j++) {
-			// Print it's status
-			printf("%s", (g->board[i][j] == 0 ? "o" : "x"));
-			// If this is the last item print a new line
-			if(j == g->size - 1){ printf("\n"); }
-		}
-	}
+    // For each row
+    for(int i = 0; i < g->size; i++) {
+        // For each column
+        for(int j = 0; j < g->size; j++) {
+            // Print it's status
+            printf("%s", (g->board[i][j] == 0 ? "." : "x"));
+        }
+        printf("\n");
+    }
 }
--- main.c
+++ main.c
@@ -1,13 +1,352 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include "game_of_life.h"
 
+// Function prototypes
+
+// Main functions
+int** create_board(size_t size);
+
+//void get_initial_pattern(gol *g);
+
+void create_glider_gun_gol(gol *g);
+
+void create_light_weight_spaceship(gol *g);
+
+void create_pulsar_gol(gol *g);
+
+void create_random_gol(gol *g);
+
+// Utilities
+void print_n(char *c);
+
+void printDash();
+
+// Main function
 int main() {
-	printf("Game of Life\n");
-	printf("-----------------------");
 
-	gol* g = create_default_gol();
+    /**
+     * TODO ->
+     * While(1)
+     *  Display menu
+     *  Take action input
+     *  Perform action
+     *  Clear console? (Stop it from getting huge)
+     * Call it a day
+     */
+    print_n("Conway's Game of Life");
+	// Setup initial variables
+	gol *g = malloc(sizeof(gol *)); // This is so we can implement destroy_gol easily
+    // This acts as a bool to stop someone from attempting to print the next step with an empty/null struct
+	int gol_initialized = 0;
+    // This acts as a bool
+	int running = 1;
+	// Start the loop
+    // While(1) is bad practice so lets avoid that by assigning it to variable
+    while(running) {
+        printDash();
+        print_n("Options");
+        printDash();
 
-	print(g);
-	return 0;
+        print_n("1. Select initial pattern");
+        print_n("2. Print next pattern");
+        print_n("3. Exit program");
+
+        // Initialise an empty choice
+        int choice;
+        // Get the choice input.
+        do {
+            printf("Enter your selection [1-3]: ");
+            // This catches the invalid input in scanf that causes an infinite loop
+            // @Source: https://stackoverflow.com/questions/25945791/character-input-in-scanfd-value
+            if(scanf("%d", &choice) == 0) {
+                printf("Invalid input caused scanf buffer overflow, exiting now...");
+                exit(1);
+            }
+        } while(choice < 1 || choice > 3);
+
+        // Use a switch statement to call actions based on the choice.
+        switch(choice) {
+            // Select initial pattern
+            case 1:
+                free(g);
+                /**
+                 * TODO: Refactor this entire case.
+                 * I previously had it in a series of methods however I was getting segfaults since my
+                 * pointers were moving for some reason?
+                 */
+                printDash();
+                print_n("Initial pattern options");
+                printDash();
+
+                int pChoice;
+
+                print_n("1. Random");
+                print_n("2. Glider Gun");
+                print_n("3. Pulsar");
+                print_n("4. Lightweight Space Ship");
+                do {
+                    printf("Enter your selection: ");
+                    if(scanf("%d", &pChoice) == 0) {
+                        printf("Invalid input caused scanf buffer overflow, exiting now...");
+                        exit(1);
+                    }
+                } while(pChoice < 1 || pChoice > 4);
+
+                switch(pChoice) {
+                    case 1:
+                        print_n("Creating random pattern...");
+                        g = create_default_gol();
+                        create_random_gol(g);
+                        break;
+                    case 2:
+                        print_n("Creating Glider Gun pattern...");
+                        g = create_gol((size_t)40);
+                        create_glider_gun_gol(g);
+                        break;
+                    case 3:
+                        print_n("Creating Pulsar pattern...");
+                        g = create_default_gol();
+                        create_pulsar_gol(g);
+                        break;
+                    case 4:
+                        print_n("Creating Lightweight Space Ship pattern...");
+                        g = create_default_gol();
+                        create_light_weight_spaceship(g);
+                        break;
+                }
+
+                print(g);
+                gol_initialized = 1;
+                break;
+            // Print next pattern (evolve/step)
+            case 2:
+                if(!gol_initialized) {
+                    print_n("You need to select a pattern first!");
+                    break;
+                }
+                next_pattern(g);
+                print(g);
+                break;
+            // Exit program
+            case 3:
+                running = 0;
+                break;
+            // This should never reach execution, if it does then congrats?
+            default:
+                print_n("How did this happen?!");
+                break;
+        }
+    }
+
+    return 0;
+}
+
+// Function implementations
+
+// This wasn't working so I canned it.
+//void get_initial_pattern(gol *g) {
+//    printDash();
+//    print_n("Initial pattern options");
+//    printDash();
+//
+//    int choice;
+//
+//    print_n("1. Random");
+//    print_n("2. Glider Gun");
+//    print_n("3. Pulsar");
+//    print_n("4. Lightweight Space Ship");
+//    do {
+//        printf("Enter your selection: ");
+//        if(scanf("%d", &choice) == 0) {
+//            printf("Invalid input caused scanf buffer overflow, exiting now...");
+//            exit(1);
+//        }
+//    } while(choice < 1 || choice > 4);
+//
+//    switch(choice) {
+//        case 1:
+//            print_n("Creating random pattern...");
+//            create_random_gol(g);
+//            break;
+//        case 2:
+//            print_n("Creating Glider Gun pattern...");
+//            break;
+//        case 3:
+//            print_n("Creating Pulsar pattern...");
+//            break;
+//        case 4:
+//            print_n("Creating Lightweight Space Ship pattern...");
+//            create_light_weight_spaceship(g);
+//            break;
+//    }
+//
+//    print(g);
+//
+//}
+
+void create_random_gol(gol *g) {
+    // Set our struct to the default values
+//    g = create_default_gol();
+    // Seed our random
+    srand((unsigned)time(0));
+    // Create a second board
+    int **board = create_board(g->size);
+    // Fill the board with values
+    for(int i = 0; i < g->size; i++) {
+        for(int j = 0; j < g->size; j++) {
+            // If rand is even then make it an alive cell, otherwise make it a dead cell
+            board[i][j] = (rand() % 2 == 0 ? 1 : 0);
+        }
+    }
+
+    // Apply the board to our gol struct
+    set_pattern(g, board);
+    // Free the board variable for garbage collection.
+    free(board);
+}
+
+void create_glider_gun_gol(gol *g) {
+    const int pSize = 40;
+    int pattern[pSize][pSize] = {
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0},
+            {0,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,1,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+    };
+
+    // I know I could just use &pattern but I want to have buffer for actions on the board
+    int **b = create_board(g->size);
+    for(int i = 0; i < pSize; i++) {
+        for(int j = 0; j < pSize; j++) {
+            b[i][j] = pattern[i][j];
+        }
+    }
+
+    set_pattern(g, b);
+
+    free(b);
+}
+
+void create_pulsar_gol(gol *g) {
+    // Create our pattern
+    int pattern[17][17] = {
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,1,1,1,0,0,0,1,1,1,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,1,0,0,0,0,1,0,1,0,0,0,0,1,0,0},
+            {0,0,1,0,0,0,0,1,0,1,0,0,0,0,1,0,0},
+            {0,0,1,0,0,0,0,1,0,1,0,0,0,0,1,0,0},
+            {0,0,0,0,1,1,1,0,0,0,1,1,1,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,1,1,1,0,0,0,1,1,1,0,0,0,0},
+            {0,0,1,0,0,0,0,1,0,1,0,0,0,0,1,0,0},
+            {0,0,1,0,0,0,0,1,0,1,0,0,0,0,1,0,0},
+            {0,0,1,0,0,0,0,1,0,1,0,0,0,0,1,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,1,1,1,0,0,0,1,1,1,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    };
+
+    int **b = create_board(g->size);
+    for(int i = 0; i < 17; i++) {
+        for(int j = 0; j < 17; j++) {
+            b[i][j] = pattern[i][j];
+        }
+    }
+
+    set_pattern(g, b);
+
+    free(b);
+}
+
+void create_light_weight_spaceship(gol *g) {
+    // Create a board of default size so there's room for movement
+//    g = create_default_gol();
+    // Create our pattern
+    int pattern[10][10] = {
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,1,0,0,1,0,0},
+            {0,0,0,1,0,0,0,0,0,0},
+            {0,0,0,1,0,0,0,1,0,0},
+            {0,0,0,1,1,1,1,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0}
+    };
+
+    int **b = create_board(g->size);
+    for(int i = 0; i < 10; i++) {
+        for(int j = 0; j < 10; j++) {
+            b[i][j] = pattern[i][j];
+        }
+    }
+
+    set_pattern(g, b);
+
+    free(b);
+}
+
+int** create_board(size_t size) {
+    int **b = malloc(sizeof(int *) * size);
+    for(int i = 0; i < size; i++) {
+        b[i] = malloc(sizeof(int *) * size);
+    }
+
+    for(int i = 0; i < size; i++) {
+        for(int j = 0; j < size; j++) {
+            b[i][j] = 0;
+        }
+    }
+
+    return b;
+}
+
+void printDash() {
+    printf("-----------------------\n");
+}
+
+void print_n(char *c) {
+    printf("%s\n", c);
 }
